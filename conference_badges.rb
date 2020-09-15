d24102b1f64fefebe7170f658db31d0381acf21c
David Mills <david_mills109@yahoo.com>
2020-09-15 23:20:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,22 +1,31 @@
-def assign_rooms(array)
-  nuarray = []
-  counter = 1
-  array.each do |name|
-    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
-  end
-  return nuarray
-end
-
-# assign_rooms(people)
-
-def printer(array)
-  batch_badge_creator(array).each do |id|
-    puts id
-  end 
-  
-  assign_rooms(array).each do |id|
-    puts id 
-  end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end 
 
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
+  
+end 
+
+def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
